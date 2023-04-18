67e9f66c0741a2a7ec7815e8593c7e50b6052854
Simon Bernabeu <simon.bernabeu@epitech.eu>
2023-04-19 00:19:26 +0200

    Get square is crashing

--- src/main.cpp
+++ src/main.cpp
@@ -61,15 +61,17 @@ int main(int ac, char **av) {
 
             if (mouseInput.isClicked(window)) {
                 Square *squareOfPossibleMove = board->getRelativeSquare(mouseInput.getRelativePositionClick());
-                if (mouseInput.getRelativePositionClick() == piece->getMemoryPosition()) {
-                    moves.clear();
-                    piece = nullptr;
-                } else if (squareOfPossibleMove != nullptr && squareOfPossibleMove->isPossibleMove()) {
-                    std::cout << "Move piece to " << mouseInput.getRelativePositionClick().x << "x"
-                              << mouseInput.getRelativePositionClick().y << std::endl;
-                    board->movePiece(piece, squareOfPossibleMove->getPosition());
-                    moves.clear();
-                }
+                std::cout << "Clicked on square: " << mouseInput.getRelativePositionClick().x << "x"
+                          << mouseInput.getRelativePositionClick().y << std::endl;
+                // if (mouseInput.getRelativePositionClick() == piece->getMemoryPosition()) {
+                //     moves.clear();
+                //     piece = nullptr;
+                // } else if (squareOfPossibleMove != nullptr && squareOfPossibleMove->isPossibleMove()) {
+                //     std::cout << "Move piece to " << mouseInput.getRelativePositionClick().x << "x"
+                //               << mouseInput.getRelativePositionClick().y << std::endl;
+                //     board->movePiece(piece, squareOfPossibleMove->getPosition());
+                //     moves.clear();
+                // }
             }
         }
 
